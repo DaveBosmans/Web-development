f8d7d9792a572db21b44cda85a41f220a06f6de8
Dave <dave.b11@hotmail.com>
2021-01-25 16:16:58 +0100

    EindOpdracht Rekenmachine
    Afgerond

--- Javascript/8.Eindopdrachten/Calculator/app.js
+++ Javascript/8.Eindopdrachten/Calculator/app.js
@@ -1,5 +1,70 @@
 
-function calculator() {
+let outputScreen = document.getElementById("output");
 
+
+// function inputButtons(pressed) {
+//     let buttonInput =+ parseFloat(pressed.innerHTML).toFixed(2);
+//     let buttonInputSigns = pressed.innerHTML;
+//     console.log(pressed.innerHTML);
+//     console.log(buttonInput);
+//     console.log("datatype " + typeof (buttonInput));
+
+//     if (buttonInputSigns.innerHTML == '=') {
+//         outputScreen.innerHTML = Number(eval(outputScreen.innerHTML));
+     
+//     } else if (buttonInputSigns == 'AC') {
+//         outputScreen.innerHTML = '';
+//     } else if (buttonInputSigns == '+/-') {
+//         outputScreen.innerHTML -= buttonInput;
+//     } 
+    
+    
+//     else {
+//         if(outputScreen.innerHTML == '0'){
+//             outputScreen.innerHTML = buttonInput;
+//         } else {
+//             outputScreen.innerHTML += buttonInput;
+//         }
+//     }
+
+// }
+
+
+
+function inputButtons(pressed) {
+    let buttonInput = pressed.innerHTML;
+    document.getElementById ("AC").innerHTML = "C";
+
+
+    if (buttonInput == '/' || buttonInput == '*' || buttonInput == '+' || buttonInput == '-' || buttonInput == '=') {
+       pressed.style.backgroundColor = "green";
+
+       setTimeout(() => {
+           pressed.style.backgroundColor = "";
+       }, 500);
+    }
+    
+
+
+    if (buttonInput == '=') {
+        outputScreen.innerHTML =+ parseFloat(eval(outputScreen.innerHTML)).toFixed(2);
+        console.log(typeof (test));
+        console.log(outputScreen);
+        console.log("datatype " + typeof (outputScreen.innerHTML));
+     
+    } else if (buttonInput == 'AC' || buttonInput == 'C') {
+        outputScreen.innerHTML = 0;
+        document.getElementById ("AC").innerHTML = "AC";
+    } else if (buttonInput == '+/-') {
+        outputScreen.innerHTML = outputScreen.innerHTML * -1;
+    } 
+    
+    else {
+        if(outputScreen.innerHTML == '0'){
+            outputScreen.innerHTML = buttonInput;
+        } else {
+            outputScreen.innerHTML += buttonInput;
+        }
+    }
 }
 
