7b2d6275bf422569aa9da045c75c8f671d7d853c
Dave <dave.b11@hotmail.com>
2021-01-29 16:54:02 +0100

    Java

---
+++ JavaLeren/ControlFlow/src/com/company/Main.java
@@ -0,0 +1,15 @@
+
+package com.company;
+import java.util.Scanner;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Pick a number between 1 and 10");
+        Scanner scanner = new Scanner(System.in);
+        int inputtedNum = scanner.nextInt();
+    }
+}
