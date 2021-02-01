b0bbbbf1ff93bc046162c1364441f58d518ad2b7
Dave <dave.b11@hotmail.com>
2021-02-01 19:52:47 +0100

    Java
    Java basis cursus afgerond

--- JavaLeren/Classes/src/com/company/Main.java
+++ JavaLeren/Classes/src/com/company/Main.java
@@ -3,17 +3,26 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Triangle triangleA = new Triangle(15,8,15,8, 17);
-        Triangle triangleB = new Triangle(3,2.598, 3 , 3, 3);
 
-        double triangleAArea = triangleA.findArea();
-        System.out.println(triangleAArea);
+//        TRIANGLE CLASS
 
-        double triangleBArea = triangleB.findArea();
-        System.out.println(triangleBArea);
+//        Triangle triangleA = new Triangle(15,8,15,8, 17);
+//        Triangle triangleB = new Triangle(3,2.598, 3 , 3, 3);
+//
+//        double triangleAArea = triangleA.findArea();
+//        System.out.println(triangleAArea);
+//
+//        double triangleBArea = triangleB.findArea();
+//        System.out.println(triangleBArea);
+//
+//        System.out.println(triangleA.sideLenThree);
+//        System.out.println(triangleB.base);
+//        System.out.println(Triangle.numsOfSides);
 
-        System.out.println(triangleA.sideLenThree);
-        System.out.println(triangleB.base);
-        System.out.println(Triangle.numsOfSides);
+        StudentProfile profileOne = new StudentProfile("Dave", "Bosmans", "Back End Developer", 5, 2021);
+        StudentProfile profileTwo = new StudentProfile("John", "Doe", "Front End Developer", 5, 2021);
+
+        profileTwo.incrementExpectedGraduationYear();
+        System.out.print(profileTwo.expectedYearToGraduate);
     }
 }
