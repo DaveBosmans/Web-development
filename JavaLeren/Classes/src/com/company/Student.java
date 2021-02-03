09f820f2f6dc979a07f9a7adc9dbccc9d843f7b2
Dave <dave.b11@hotmail.com>
2021-02-03 17:00:08 +0100

    Java

---
+++ JavaLeren/Classes/src/com/company/Student.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Student {
+    String firstName;
+    String lastName;
+    String declaredMajor;
+    double gpa;
+    int expectedYearToGraduate;
+
+
+public Student(String studentName){
+    this.firstName = studentName;
+}
+}
