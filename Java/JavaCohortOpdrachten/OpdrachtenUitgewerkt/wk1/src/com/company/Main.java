ea691ef0c51def6c358de6d83524cd324d8719b3
Dave <dave.b11@hotmail.com>
2021-02-08 17:06:09 +0100

    Java Cohort Opdrachten
    Opdracht 1

---
+++ Java/JavaCohortOpdrachten/OpdrachtenUitgewerkt/wk1/src/com/company/Main.java
@@ -0,0 +1,62 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        //--------------ANTWOORDEN OPDRACHT 1
+//        Opdracht 1 - Basic Syntax
+//
+//        Voordat je Java kan gebruiken moet je de syntax kunnen begrijpen. Doe onderzoek naar de
+//        volgende termen en leg uit wat ze (voor Java) betekenen:
+
+//         Object: An object is a software bundle of variables and related methods. You can
+//        represent real-world objects using software objects.
+
+//        Class: A Class is like an object constructor, or a "blueprint" for creating objects.
+//
+//        Package: A package is a namespace that organizes a set of related classes and
+//        interfaces.
+
+//        Constructor: A constructor in Java is a special method that is used to initialize
+//        objects. The constructor is called when an object of a class is created. It can be used
+//        to set initial values for object attributes.
+
+//        Instance: An object and an instance are the same thing.
+
+//        Method: A method is a block of code which only runs when it is called.
+//        You can pass data, known as parameters, into a method.
+//        Methods are used to perform certain actions, and they are also known as functions.
+//        Why use methods? To reuse code: define the code once, and use it many times.<br>
+//                Class > Class Constructor > Method > Initialized
+
+        //----------------EINDE OPDRACHT 1
+
+//------------------ START OPDRACHT 2
+
+//        Opdracht 2 - Variables
+//
+//        Ook variables werken in Java net iets anders dan je hiervoor misschien gewend bent. Java maakt onderscheid tussen 3 soorten variables:
+//        Local variables
+//        Instance variables
+//        Class variables (ook wel Static variables genoemd)
+//
+//        Wat voor soort variabele je gebruikt hangt onder andere af van de plek waar je hem declareert .
+//       a) Waar wordt een local variable gedeclareerd?
+//        Local variables are declared in methods, constructors, or blocks.
+//
+//       Class/Static variables en Instance variables worden op dezelfde plek gedeclareerd, en houden ook allebei de gegevens van een Object bij.
+//       b) Wat voor soort informatie sla je op in een instance variable?
+//       Instance variables are declared in a class, but outside a method, constructor or any block.
+
+//       c) Wat voor soort informatie sla je op in een class variable?
+        
+//
+//       De waarde van een class variable kan je in principe ook opslaan in een instance variable.
+//       d) Wat is het voordeel van het gebruiken van een class variable?
+
+
+
+    }
+}
