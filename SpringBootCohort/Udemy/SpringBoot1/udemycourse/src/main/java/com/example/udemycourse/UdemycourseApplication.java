4549654c17dd3e8b391cded06bb75c7005094035
Dave <dave.b11@hotmail.com>
2021-03-15 16:53:53 +0100

    Spring Boot Udemy

--- SpringBootCohort/Udemy/SpringBoot1/udemycourse/src/main/java/com/example/udemycourse/UdemycourseApplication.java
+++ SpringBootCohort/Udemy/SpringBoot1/udemycourse/src/main/java/com/example/udemycourse/UdemycourseApplication.java
@@ -2,6 +2,7 @@ package com.example.udemycourse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class UdemycourseApplication {
@@ -9,5 +10,6 @@ public class UdemycourseApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UdemycourseApplication.class, args);
 	}
+
 
 }
