bf97d125296798deec0cd9bcb00bb5225efabdfb
Dave <dave.b11@hotmail.com>
2021-03-22 11:26:45 +0100

    Cohort back end webshop

---
+++ SpringBootCohort/CohortWebshop/webshop/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO TBL_WEBSHOP (product_name, product_description, product_stock) values
+('bonbonsdoosje', 'Heerlijke bonbons gemix', 10);
+('paashaas', 'Paashaas 20cm', 20);
+('kuiken uit ei', 'leuke paaskuiken die uit een ei komt gemaakt van witte chocolade', 10);
