77e4ac8992bf00ebc62435e77021bf3a7c6de1cd
Dave <dave.b11@hotmail.com>
2021-04-07 14:24:16 +0200

    Netflix Project

---
+++ NetflixProjectV1/netflixproject/src/main/java/com/example/netflixproject/NetflixprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.netflixproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixprojectApplication.class, args);
+	}
+
+}
---
+++ NetflixProjectV1/netflixproject/src/test/java/com/example/netflixproject/NetflixprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.netflixproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
