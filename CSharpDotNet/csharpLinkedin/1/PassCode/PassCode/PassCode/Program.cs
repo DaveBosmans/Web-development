3a070ef5f652900829b07a5dda089e6639352a03
Dave <dave.b11@hotmail.com>
2021-04-13 07:33:20 +0200

    C Sharp .Net
    Conditions Simple PassCode App

---
+++ CSharpDotNet/csharpLinkedin/1/PassCode/PassCode/PassCode/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PassCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is you passcode?");
+            var passcode = Console.ReadLine();
+
+            if(passcode == "secret")
+            {
+                Console.WriteLine("You are Authenticated.");
+            } else if  (passcode != "secret")
+            {
+                Console.WriteLine("Wrong password.");
+            }
+        }
+    }
+}
