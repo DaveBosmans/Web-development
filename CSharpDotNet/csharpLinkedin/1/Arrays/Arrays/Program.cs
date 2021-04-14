7a23a968d9af62bb0555bb799a9530abea148000
Dave <dave.b11@hotmail.com>
2021-04-14 07:33:55 +0200

    CSharp .Net
    Arrays ForEach

---
+++ CSharpDotNet/csharpLinkedin/1/Arrays/Arrays/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var studentGrades = new int[] { 3, 5, 3, 8, 9, 10, 8, 6, 5, 7, 1};
+
+            foreach (var studentGrade in studentGrades)
+            {
+                Console.WriteLine(studentGrade);
+            }
+         
+        }
+    }
+}
