41ab3ecc597a19c8e5eff338f7b2a499db1afcc7
Dave <dave.b11@hotmail.com>
2021-04-14 06:50:31 +0200

    C Sharp .Net
    C# While Loop

---
+++ CSharpDotNet/csharpLinkedin/1/Loops/Loops/Loops/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var code = "";
+            while (code != "secret") {
+                Console.WriteLine("What is your passcode?");
+                code = Console.ReadLine();
+
+                if (code != "secret")
+                {
+                    Console.WriteLine("Wrong Passcode");
+                }
+            }
+            Console.WriteLine("You are Authenticated");
+        }
+    }
+}
