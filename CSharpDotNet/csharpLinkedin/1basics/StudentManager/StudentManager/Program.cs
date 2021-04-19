407bff1717b78a1bd66ec3be9e3f2c3ea4f43fc1
Dave <dave.b11@hotmail.com>
2021-04-19 20:12:47 +0200

    C Sharp .Net
    StudentManager

--- CSharpDotNet/csharpLinkedin/1basics/StudentManager/StudentManager/Program.cs
+++ CSharpDotNet/csharpLinkedin/1basics/StudentManager/StudentManager/Program.cs
@@ -6,7 +6,28 @@ namespace StudentManager
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("How many students in your class?");
+            var studentCount = int.Parse(Console.ReadLine());
+            var studentNames = new string[studentCount];
+            var studentGrades = new int[studentCount];
+
+
+            for (int i = 0; i < studentCount; i++)
+            {
+                Console.Write("Student Name: ");
+                studentNames[i] = Console.ReadLine();
+
+                Console.Write("Student 'Grade: ");
+                studentGrades[i] = int.Parse(Console.ReadLine());
+
+            }
+
+            for (int i = 0; i < studentCount; i++)
+            {
+                Console.WriteLine("Name: {0}, Grade: {1}", studentNames[i], studentGrades[i]);
+            }
+
+
         }
     }
 }
