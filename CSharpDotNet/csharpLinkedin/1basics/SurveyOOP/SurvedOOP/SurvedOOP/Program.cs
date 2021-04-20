d23cfc7dba1733363800aaa59db1b45a1dc80d61
Dave <dave.b11@hotmail.com>
2021-04-20 13:58:15 +0200

    C Sharp .Net
    OOP Survey Opdracht

---
+++ CSharpDotNet/csharpLinkedin/1basics/SurveyOOP/SurvedOOP/SurvedOOP/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace SurvedOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newSurvey = new Data();
+            newSurvey.Name = Util.Console.Ask("What is your name?");
+            newSurvey.Age = int.Parse(Util.Console.Ask("What is your age?"));
+            newSurvey.Month = Util.Console.Ask("What month were you born in?");
+
+            newSurvey.Display();
+
+            if (newSurvey.Month == "march")
+            {
+                Console.WriteLine("You are an Aries.");
+            }
+            else if (newSurvey.Month == "april")
+            {
+                Console.WriteLine("You are a Taurus.");
+            }
+            else if (newSurvey.Month == "may")
+            {
+                Console.WriteLine("You are a Gemini");
+            }
+        }
+
+
+        class Data {
+            public string Name;
+            public int Age;
+            public string Month;
+
+            public void Display()
+            {
+                Console.WriteLine("Your name is: {0}", Name);
+                Console.WriteLine("Your age is: {0}", Age);
+                Console.WriteLine("You are born in: {0}", Month);
+            }
+
+        }
+    }
+}
